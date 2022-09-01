c4e698475afae906b7ea2974fd427708fadf5de2
Katie Cleland <106635778+kcleland0818@users.noreply.github.com>
2022-08-31 18:58:10 -0600

    Create a clearer lab (#1)
    * Create a clearer lab
    
    * Added clearer instructions
    
    * Use imperative sentences

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,26 +1,14 @@
-# User Interaction Lab
-
-## Learning Goals
-
-- Take user input
-- Process and display user input
-
-## Instructions
-
-Let's go back to our `Student` class example!
-
-```java
 public class Student {
     private String name;
-    
+
     Student(String name) {
         this.name = name;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
@@ -41,20 +29,8 @@ public class Student {
             System.out.println("Oops! Better luck next time!");
         }
     }
-    
+
     public static void main(String args[]) {
         // Write Student user input here
     }
 }
-```
-
-Let's ask the user for input and use our `Student` class! 
-
-First, prompt the user for their name and then create a `Student` instance with
-that name.
-
-Second, greet the user by saying, "Hi <name>! What grade did you get on the
-test?" This will prompt the user to enter in an integer value for a grade.
-
-Third, call the method `calculateGrade()` which will print a message based on
-the grade the user typed in.
